6038468ac5b25fc70a931905c4eac4e646f849ac
WL <walldi@tlen.pl>
2020-02-29 13:46:05 +0100

    zapis listy studentow

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -27,18 +27,10 @@ public class Main {
         studentList.add(student4);
         studentList.add(student5);
 
-        try {
-            objectMapper.writeValue(new File("studentList.json"), studentList);
-            objectMapper.writeValue(new File("student.json"), student);
-            objectMapper.writeValue(new File("student1.json"), student1);
-            objectMapper.writeValue(new File("student2.json"), student2);
-            objectMapper.writeValue(new File("student3.json"), student3);
-            objectMapper.writeValue(new File("student4.json"), student4);
-            objectMapper.writeValue(new File("student5.json"), student5);
+        JSONUtils jsonUtils = new JSONUtils();
+        jsonUtils.writeList("lista.json", studentList);
+        jsonUtils.readList("lista.json");
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
 }
