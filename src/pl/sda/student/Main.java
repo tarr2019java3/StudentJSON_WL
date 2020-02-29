232382d08c1bf97e6d61a63317522dccf7ec6c50
WL <walldi@tlen.pl>
2020-02-29 11:36:39 +0100

    zapis obiektu do json

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -1,7 +1,22 @@
 package pl.sda.student;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
+
     public static void main(String[] args) {
 
+        Student student = new Student("Jan", "Kowalski", 32);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.writeValue(new File("student.json"), student);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
